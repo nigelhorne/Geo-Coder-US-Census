2fc7deb7bddc8b21050b36ea45e8f1d4ebe871e8
Nigel Horne <njh@bandsman.co.uk>
2023-06-23 13:22:26 -0400

    TODO in t/us.t is now working

--- lib/Geo/Coder/US/Census.pm
+++ lib/Geo/Coder/US/Census.pm
@@ -18,11 +18,11 @@ Geo::Coder::US::Census - Provides a Geo-Coding functionality for the US using L<
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 =head1 SYNOPSIS
 
